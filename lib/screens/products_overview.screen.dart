ec08f3f3ce21a9635928afb1bf54db346d90d8a1
JulioHey <juliohey.dev@gmail.com>
2021-03-21 21:12:38 -0300

    Added some base Screens and models
     - Refactoring of main.dart;
     - Products model addde;
     - Products Overview Screen added;

---
+++ lib/screens/products_overview.screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../models/product.model.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    return Container();
+  }
+}
