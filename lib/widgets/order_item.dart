1baf42b040c857b7249c9ad668bb5bc02733f795
JulioHey <juliohey.dev@gmail.com>
2021-03-23 10:35:57 -0300

    Added ListView to show the orders
     - Added listView to show Orders;
     - Transformed OrderItem in StatefulWidget to enable the expand button to show more information;

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.provider.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(
@@ -11,23 +13,66 @@ class OrderItem extends StatelessWidget {
   );
 
   @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _expanded = false;
+  
+  @override
   Widget build (BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(10),
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${order.amount}'),
+            title: Text('\$${widget.order.amount}'),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(order.datetime)
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.datetime)
             ),
             trailing: IconButton(
               icon: Icon(
-                Icons.expand_more
+                _expanded ? Icons.expand_less: Icons.expand_more
               ),
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             )
-          )
+          ),
+          if (_expanded) Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 4,
+            ),
+            height: min(widget.order.products.length * 20.0 + 10, 100),
+            child: ListView(
+              children: widget.order.products
+              .map(
+                (product) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        product.title,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        )
+                      ),
+                      Text(
+                        '${product.quantity} x ${product.price}',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        )
+                      )
+                    ]
+                  );
+              }).toList(),
+            )
+          ),
         ]
       )
     );
